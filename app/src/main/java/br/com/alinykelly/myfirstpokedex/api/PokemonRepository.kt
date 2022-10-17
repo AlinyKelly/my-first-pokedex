08d2d617e65c68f7f9c56b4ac11c29c3c3e2224f
Aliny Kelly <alinykellyfs@gmail.com>
2022-10-17 00:04:29 -0300

    atualizacao do PokemonRepository para object

--- app/src/main/java/br/com/alinykelly/myfirstpokedex/api/PokemonRepository.kt
+++ app/src/main/java/br/com/alinykelly/myfirstpokedex/api/PokemonRepository.kt
@@ -1,7 +1,43 @@
 package br.com.alinykelly.myfirstpokedex.api
 
-class PokemonRepository {
-    //https://pokeapi.co/api/v2/pokemon/?limit=151
+import android.util.Log
+import br.com.alinykelly.myfirstpokedex.api.model.PokemonService
+import br.com.alinykelly.myfirstpokedex.api.model.PokemonsApiResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+object PokemonRepository {
+    private val service: PokemonService
+
+    //https://pokeapi.co/api/v2/pokemon/?limit=151
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    service = retrofit.create(PokemonService::class.java)
+    }
+
+    fun listPokemons(limit: Int = 151) {
+        val call = service.listPokemons(limit)
+
+        call.enqueue(object : Callback<PokemonsApiResult> {
+            override fun onResponse(
+                call: Call<PokemonsApiResult>,
+                response: Response<PokemonsApiResult>
+            ) {
+                Log.d("POKEMON_API", "Pokemons list loaded")
+            }
+
+            override fun onFailure(call: Call<PokemonsApiResult>, t: Throwable) {
+                Log.e("POKEMON_API", "Error loading pokemons list.", t)
+            }
+
+        })
+    }
 
 }
