296641023389ae3ffa62fc953d5c497237effe80
Aliny Kelly <alinykellyfs@gmail.com>
2022-10-16 23:27:56 -0300

    Implementacao para Resultado Individuais de pokemons

--- app/src/main/java/br/com/alinykelly/myfirstpokedex/api/model/PokemonsApiResult.kt
+++ app/src/main/java/br/com/alinykelly/myfirstpokedex/api/model/PokemonsApiResult.kt
@@ -1,5 +1,8 @@
 package br.com.alinykelly.myfirstpokedex.api.model
 
+import br.com.alinykelly.myfirstpokedex.domain.PokemonType
+
+//Listagem de todos os pokemons
 data class PokemonsApiResult (
     val count: Int,
     val next: String?,
@@ -10,4 +13,16 @@ data class PokemonsApiResult (
 data class PokemonResult(
     val name: String,
     val url: String
+)
+
+//Resultado para apenas 1 pokemon
+data class PokemonApiResult(
+    val id: Int,
+    val name: String,
+    val types: PokemonTypeSlot
+)
+
+data class PokemonTypeSlot(
+    val slot: Int,
+    val type: PokemonType
 )
