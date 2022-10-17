8bfd71bcbf5faecf73a437cbddde49d28787e053
Aliny Kelly <alinykellyfs@gmail.com>
2022-10-16 23:29:25 -0300

    Criacao do GET para retorno da lista com limite e posicao do pokemon na lista

---
+++ app/src/main/java/br/com/alinykelly/myfirstpokedex/api/model/PokemonService.kt
@@ -0,0 +1,14 @@
+package br.com.alinykelly.myfirstpokedex.api.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PokemonService {
+    @GET("pokemon")
+    fun listPokemons(@Query("limit") limit: Int): Call<PokemonsApiResult>
+
+    @GET("pokemon/{number}")
+    fun getPokemon(number: Int): Call<PokemonApiResult>
+
+}
