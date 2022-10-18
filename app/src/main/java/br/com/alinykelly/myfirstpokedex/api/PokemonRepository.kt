0b79e1d4093dcd5b64b29265b0697919043c87d3
Aliny Kelly <alinykellyfs@gmail.com>
2022-10-17 23:23:15 -0300

    Criacao da Thread

--- app/src/main/java/br/com/alinykelly/myfirstpokedex/api/PokemonRepository.kt
+++ app/src/main/java/br/com/alinykelly/myfirstpokedex/api/PokemonRepository.kt
@@ -1,11 +1,10 @@
 package br.com.alinykelly.myfirstpokedex.api
 
-import android.util.Log
+
+import br.com.alinykelly.myfirstpokedex.api.model.PokemonApiResult
 import br.com.alinykelly.myfirstpokedex.api.model.PokemonService
 import br.com.alinykelly.myfirstpokedex.api.model.PokemonsApiResult
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import br.com.alinykelly.myfirstpokedex.domain.Pokemon
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -22,22 +21,38 @@ object PokemonRepository {
     service = retrofit.create(PokemonService::class.java)
     }
 
-    fun listPokemons(limit: Int = 151) {
+    fun listPokemons(limit: Int = 151): PokemonsApiResult? {
         val call = service.listPokemons(limit)
 
-        call.enqueue(object : Callback<PokemonsApiResult> {
-            override fun onResponse(
-                call: Call<PokemonsApiResult>,
-                response: Response<PokemonsApiResult>
-            ) {
-                Log.d("POKEMON_API", "Pokemons list loaded")
-            }
+        return call.execute().body()
 
-            override fun onFailure(call: Call<PokemonsApiResult>, t: Throwable) {
-                Log.e("POKEMON_API", "Error loading pokemons list.", t)
-            }
+//        call.enqueue(object : Callback<PokemonsApiResult> {
+//            override fun onResponse(
+//                call: Call<PokemonsApiResult>,
+//                response: Response<PokemonsApiResult>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val body = response.body()
+//
+//                    body?.results?.let {
+//                        Log.d("POKEMON_API", it[0].name)
+//                    }
+//                }
+//
+//                Log.d("POKEMON_API", "Pokemons list loaded")
+//            }
+//
+//            override fun onFailure(call: Call<PokemonsApiResult>, t: Throwable) {
+//                Log.e("POKEMON_API", "Error loading pokemons list.", t)
+//            }
+//
+//        })
+    }
 
-        })
+    fun getPokemon(number: Int): PokemonApiResult? {
+        val call = service.getPokemon(number)
+
+        return call.execute().body()
     }
 
 }
--- app/src/main/java/br/com/alinykelly/myfirstpokedex/view/MainActivity.kt
+++ app/src/main/java/br/com/alinykelly/myfirstpokedex/view/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.alinykelly.myfirstpokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alinykelly.myfirstpokedex.R
@@ -10,28 +11,54 @@ import br.com.alinykelly.myfirstpokedex.domain.Pokemon
 import br.com.alinykelly.myfirstpokedex.domain.PokemonType
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rvPokemons)
+        recyclerView = findViewById(R.id.rvPokemons)
 
-        val charmander = Pokemon(
-            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-            4,
-            "Charmander",
-            listOf(
-                PokemonType("Fire")
-            )
-        )
-        val pokemons = listOf(
-            charmander, charmander, charmander, charmander, charmander
-        )
+//        val charmander = Pokemon(
+//            "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+//            4,
+//            "Charmander",
+//            listOf(
+//                PokemonType("Fire")
+//            )
+//        )
+//        val pokemons = listOf(
+//            charmander, charmander, charmander, charmander, charmander
+//        )
 
-        val pokemonsApi = PokemonRepository.listPokemons()
+        //Criar a propria thread pra fazer a requisição
+        Thread(Runnable {
+            loadPokemons()
+        }).start()
 
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = PokemonAdapter(pokemons)
+    }
+
+    private fun loadPokemons() {
+       val pokemonsApiResult = PokemonRepository.listPokemons()
+
+        pokemonsApiResult?.results?.let {
+
+            val pokemons: List<Pokemon> = it.map {
+                pokemonResult ->
+                Pokemon(
+                    "",
+                    1,
+                    pokemonResult.name,
+                    listOf(PokemonType("Fire"))
+                )
+            }
+
+            val layoutManager = LinearLayoutManager(this)
+
+            recyclerView.post {
+                recyclerView.layoutManager = layoutManager
+                recyclerView.adapter = PokemonAdapter(pokemons)
+            }
+        }
     }
 }
